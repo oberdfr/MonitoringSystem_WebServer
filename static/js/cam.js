39a35aaea085d6ac2a3162d2af8b0ff2d43de0a1
oberdfr <alebennella@gmail.com>
2024-06-10 18:06:59 +0200

    status 2: revert

--- static/js/cam.js
+++ static/js/cam.js
@@ -7,53 +7,61 @@ function logout() {
         .catch(error => console.log("Error logging out:", error));
 }
 
+for (let i = 1; i < 3; i++) {
+    document.addEventListener("DOMContentLoaded", function () {
+        // Crea la card
+        var card = document.createElement('div');
 
-document.addEventListener("DOMContentLoaded", function () {
-    // Crea la card
-    var card = document.createElement('div');
-    card.className = 'card';
+        if (i == 1) {
+            card.className = 'cardLeft';
+        } else if (i == 2) {
+            card.className = 'cardLeft';
+        }
 
-    // Crea l'iframe
-    var iframe = document.createElement('iframe');
-    iframe.src = 'http://192.168.100.204:8000/bgr';
-    iframe.allow = 'autoplay';
-    iframe.width = 640
-    iframe.height = 360
 
-    var cardIframeContainer = document.createElement('div');
-    cardIframeContainer.classList.add('iframeContainer');
-    // Aggiungi l'iframe alla card
-    cardIframeContainer.appendChild(iframe);
-    card.appendChild(cardIframeContainer);
+        // Crea l'iframe
+        var iframe = document.createElement('iframe');
+        iframe.src = 'http://192.168.100.204:8000/bgr';
+        iframe.allow = 'autoplay';
+        iframe.width = 1920
+        iframe.height = 1080
 
-    // Crea il corpo della card
-    var cardBody = document.createElement('div');
-    cardBody.className = 'card-body';
+        var cardIframeContainer = document.createElement('div');
+        cardIframeContainer.classList.add('iframeContainer');
+        // Aggiungi l'iframe alla card
+        cardIframeContainer.appendChild(iframe);
+        card.appendChild(cardIframeContainer);
 
-    // Crea il titolo della card
-    var cardTitle = document.createElement('h5');
-    cardTitle.className = 'card-title';
-    cardTitle.textContent = 'Card title';
+        // Crea il corpo della card
+        var cardBody = document.createElement('div');
+        cardBody.className = 'card-body';
 
-    // Crea il testo della card
-    var cardText = document.createElement('p');
-    cardText.className = 'card-text';
-    cardText.textContent = 'Some quick example text to build on the card title and make up the bulk of the card\'s content.';
+        // Crea il titolo della card
+        var cardTitle = document.createElement('h5');
+        cardTitle.className = 'card-title';
+        cardTitle.textContent = 'Card title';
 
-    // Crea il pulsante della card
-    var cardButton = document.createElement('a');
-    cardButton.className = 'card-btn';
-    cardButton.href = '#';
-    cardButton.textContent = 'Go somewhere';
+        // Crea il testo della card
+        var cardText = document.createElement('p');
+        cardText.className = 'card-text';
+        cardText.textContent = 'Some quick example text to build on the card title and make up the bulk of the card\'s content.';
 
-    // Aggiungi il titolo, il testo e il pulsante al corpo della card
-    cardBody.appendChild(cardTitle);
-    cardBody.appendChild(cardText);
-    cardBody.appendChild(cardButton);
+        // Crea il pulsante della card
+        var cardButton = document.createElement('a');
+        cardButton.className = 'card-btn';
+        cardButton.href = '#';
+        cardButton.textContent = 'Go somewhere';
 
-    // Aggiungi il corpo della card alla card
-    card.appendChild(cardBody);
+        // Aggiungi il titolo, il testo e il pulsante al corpo della card
+        cardBody.appendChild(cardTitle);
+        cardBody.appendChild(cardText);
+        cardBody.appendChild(cardButton);
 
-    // Aggiungi la card al container
-    document.getElementById('card-container').appendChild(card);
-});
+        // Aggiungi il corpo della card alla card
+        card.appendChild(cardBody);
+
+        // Aggiungi la card al container
+        document.querySelector('#card-container').append(card);
+    });
+
+}
