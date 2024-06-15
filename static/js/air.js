4b708b7cd2275771a9dd458360fe84fa0e0d40ba
Oberd <112550174+oberdfr@users.noreply.github.com>
2024-06-15 15:28:24 +0200

    website: add initial responsive logic for air page

--- static/js/air.js
+++ static/js/air.js
@@ -2,6 +2,48 @@ var co2 = document.getElementById('co2')
 var temp = document.getElementById('temp')
 var qualita = document.getElementById('qualita')
 var piove = document.getElementById('piove')
+let primoPianoContainer = document.getElementById('primoPianoContainer')
+let seminterratoContainer = document.getElementById('seminterratoContainer')
+let airRow1 = document.getElementById('airRow1')
+let airRow2 = document.getElementById('airRow2')
+
+function isMobileDevice() {
+    console.log(/Mobi|Android|iPad|iPhone|iPod/.test(navigator.userAgent));
+    return /Mobi|Android|iPad|iPhone|iPod/.test(navigator.userAgent);
+}
+
+const DESKTOP_BIG = 1;
+const DESKTOP_LITTLE = 2;
+const MOBILE = 3;
+
+var windowWidth = window.innerWidth;
+var webWiew = 4;
+
+function updateWindowWidth() {
+
+    windowWidth = window.innerWidth;
+
+    if ((isMobileDevice() || windowWidth <= 1000) && webWiew != MOBILE) {
+        if (isMobileDevice()) {
+
+        }
+        airRow2.appendChild(seminterratoContainer);
+        webWiew = MOBILE;
+    }
+
+    if (windowWidth <= 1550 && windowWidth > 1000 && webWiew != DESKTOP_LITTLE) {
+        airRow2.appendChild(seminterratoContainer);
+        webWiew = DESKTOP_LITTLE;
+    }
+
+    if (windowWidth > 1550 && webWiew != DESKTOP_BIG) {
+        airRow1.appendChild(seminterratoContainer);
+        webWiew = DESKTOP_BIG;
+    }
+}
+
+document.addEventListener('DOMContentLoaded', updateWindowWidth);
+window.addEventListener('resize', updateWindowWidth);
 
 function logout() {
     fetch("http://192.168.68.63:5000/logout", { credentials: 'include' })
